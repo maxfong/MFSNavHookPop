144c3f9fe09889244a552b83b9f3cf385bc11966
Witty Young <maxfong@users.noreply.github.com>
2016-08-19 15:37:28 +0800

    Update UINavigationController+MFSPopOut.m

--- MFSNavigationController/UINavigationController+MFSPopOut/UINavigationController+MFSPopOut.m
+++ MFSNavigationController/UINavigationController+MFSPopOut/UINavigationController+MFSPopOut.m
@@ -22,16 +22,9 @@
     Method originalMethod = class_getInstanceMethod(self, originalSelector);
     Method newMethod = class_getInstanceMethod(self, newSelector);
     
-    BOOL methodAdded = class_addMethod([self class],
-                                       originalSelector,
-                                       method_getImplementation(newMethod),
-                                       method_getTypeEncoding(newMethod));
-    
+    BOOL methodAdded = class_addMethod([self class], originalSelector, method_getImplementation(newMethod), method_getTypeEncoding(newMethod));
     if (methodAdded) {
-        class_replaceMethod([self class],
-                            newSelector,
-                            method_getImplementation(originalMethod),
-                            method_getTypeEncoding(originalMethod));
+        class_replaceMethod([self class], newSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
     }
     else {
         method_exchangeImplementations(originalMethod, newMethod);
